6ae760f2fc4fe3e14521cd1dbb7fa50f6de85c27
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-02-13 09:57:22 +0600

    updateFunction added

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-document.getElementById('case-plus').addEventListener('click',function(){
+// using function 
+
+function updateCaseNumber(isIncreasing){
     const caseInput=document.getElementById('case-numbr');
     const caseNumber= caseInput.value;
+    if(isIncreasing==true){
     caseInput.value=parseInt(caseNumber)+1;
+
+    }else{
+    caseInput.value=parseInt(caseNumber)-1;
+    if(caseNumber<1){
+        caseInput.value=0;
+    }
+    }
+}
+
+
+document.getElementById('case-plus').addEventListener('click',function(){
+    // const caseInput=document.getElementById('case-numbr');
+    // const caseNumber= caseInput.value;
+    // caseInput.value=parseInt(caseNumber)+1;
+    updateCaseNumber(true); //using function
     
 })
 // button-minus 
 
 document.getElementById('button-minus').addEventListener('click',function(){
-    const caseInput=document.getElementById('case-numbr');
-    const caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)-1;
-    if(caseNumber<1){
-        caseInput.value=0;
-    }
+    // const caseInput=document.getElementById('case-numbr');
+    // const caseNumber= caseInput.value;
+    // caseInput.value=parseInt(caseNumber)-1;
+    // if(caseNumber<1){
+    //     caseInput.value=0;
+    // }
+    updateCaseNumber(false);
     
 })
