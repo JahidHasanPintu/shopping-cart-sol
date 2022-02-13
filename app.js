7ea24e1da963963f2b46f733c535a09d0da3e72c
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-02-13 09:49:15 +0600

    case number increment and decrement function added

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('I am in a separate file')
+document.getElementById('case-plus').addEventListener('click',function(){
+    const caseInput=document.getElementById('case-numbr');
+    const caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+})
+// button-minus 
+
+document.getElementById('button-minus').addEventListener('click',function(){
+    const caseInput=document.getElementById('case-numbr');
+    const caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)-1;
+    if(caseNumber<1){
+        caseInput.value=0;
+    }
+    
+})
